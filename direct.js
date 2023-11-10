da3134aaddb71d5214890f67fd866295fd192e98
thiagosoriedem <tms@academico.ufpb.br>
2023-11-10 00:20:45 -0300

    home login concluido

--- direct.js
+++ direct.js
@@ -4,3 +4,12 @@ function login() {
 function cadastro() {
     window.location.href = './cadastro/index.html';
 }
+function back() {
+    window.history.back();
+}
+
+// page login > adicionando dinamicametne input nas lacunas
+var input_email_cpf_login = document.getElementById('email_cpf_login');
+
+
+
