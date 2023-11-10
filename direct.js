64e2c51fad1ee89ef95b70d5f8fb0a75113e81f4
thiagosoriedem <tms@academico.ufpb.br>
2023-11-10 12:28:38 -0300

    Update cadastro link in direct.js

--- direct.js
+++ direct.js
@@ -2,11 +2,8 @@ function login() {
     window.location.href = './login/index.html';
 }
 function cadastro() {
-    window.location.href = './cadastro/index.html';
+    window.location.href = './cadastro/cadastro.html';
 }
 function back() {
     window.location.href = '../index.html';
 }
-
-
-
